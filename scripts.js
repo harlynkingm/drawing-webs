43c3fd20432cda7dd8b0aaeba0c04f3808c6d30a
Max Harlynking <mharlynk@andrew.cmu.edu>
2015-10-07 12:01:46 -0400

    initial commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    var canvas = document.getElementById('main');
+    var context = canvas.getContext('2d');
+    context.strokeStyle = "#333333";
+    context.lineCap = 'round';
+    context.lineJoin = 'round';
+    context.lineWidth = 5;
+};
